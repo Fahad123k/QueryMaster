00237774f3a3b4e08cf18ecca38b2dd1f07ff468
nisu.ahmad <nisu.ahmad@samarth.ac.in>
2023-12-07 18:07:36 +0530

    add  user info in dashboard

--- server/controller/dashboardController.js
+++ server/controller/dashboardController.js
@@ -1,7 +1,10 @@
+const mongoose=require('mongoose');
+const Notes= require('../models/Notes');
 exports.dashboard = async (req, res) => {
     const locals = {
             description: "query app write ypur code",
-            title: "Dashboard"
+            title: "Dashboard",
+            user:req.user
         }
         res.render('dashboard/index', {
             locals,
