e8bd1ade627306e435dd10a8c99136c3d8192d24
nisu.ahmad <nisu.ahmad@samarth.ac.in>
2023-12-07 15:31:57 +0530

    add google authenticaton

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 require('dotenv').config();
+
 const express= require("express");
 const expressLayout=require("express-ejs-layouts");
+const connectDB= require('./server/config/db');
+const session=require('express-session');
+const passport=require('passport');
+const MongoStore=require('connect-mongo');
 const app=express();
 
 const PORT=7000|| process.env.PORT;
 
+app.use(session({
+    secret:"keyboard cat",
+    resave:false,
+    saveUninitialized:true,
+    store:MongoStore.create({
+        mongoUrl:process.env.MONGODB_URI
+    })
+}))
 // middlewere
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-const connectDB= require('./server/config/db');
+
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 connectDB();
 
 // connect to db
@@ -22,8 +40,10 @@ app.set('layout','./layouts/main');
 app.set('view engine','ejs');
 
 // Router here
+app.use('/',require('./server/routes/auth'))
 app.use('/',require('./server/routes/index'))
 app.use('/',require('./server/routes/dashboard'))
+
 // app.get('*',(req,res)=> res.status(404).send("404 Page not found"));
 app.get('*',(req,res)=> res.status(404).render('404'));
 
---
+++ server/routes/auth.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router()
+const passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require("../models/user");
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALL_BACK_URL
+},
+    async function (accessToken, refreshToken, profile, done) {
+     
+        // console.log(profile)
+        const newUser={
+            googleId:profile.id,
+            displayName:profile.displayName,
+            firstName:profile.name.givenName,
+            lastName:profile.name.familyName,
+            profileImage:profile.photos[0].value
+
+        }
+
+        try {
+            let user=await User.findOne({ googleId:profile.id});
+            if(user){
+                done(null,user);
+                console.log("user is alredy present")
+            }
+            else{
+                user= await User.create(newUser);
+                console.log("user created",user)
+                done(null,user);
+                
+            }
+
+            
+        } catch (error) {
+            console.log("some error on ",error)            
+        }
+    }
+
+));
+
+
+
+// Google Login Route
+router.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["email", "profile"] })
+  );
+
+// retrive user's data
+router.get(
+    "/google/callback",
+    passport.authenticate("google", {
+      failureRedirect: "/login-failure",
+      successRedirect: "/dashboard",
+    })
+  );
+
+router.get('/login-failure', (req, res) => {
+    res.send('Something went wrong...');
+  });
+
+// Presist user data after successful authentication
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+});
+
+// Retrive user data from session
+// New
+passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
+module.exports = router;
