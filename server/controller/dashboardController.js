6db071e14dd3db1cca1702239fadf3ef8660c1e2
nisu.ahmad <nisu.ahmad@samarth.ac.in>
2023-12-08 17:36:08 +0530

    add notes to dashboard

--- server/controller/dashboardController.js
+++ server/controller/dashboardController.js
@@ -1,14 +1,26 @@
-const mongoose=require('mongoose');
-const Notes= require('../models/Notes');
+const mongoose = require('mongoose');
+const Notes = require('../models/Notes');
 exports.dashboard = async (req, res) => {
     const locals = {
-            description: "query app write ypur code",
-            title: "Dashboard",
-            user:req.user
-        }
+        description: "query app write ypur code",
+        title: "Dashboard",
+        user: req.user
+    }
+
+    try {
+
+        const notes = await Notes.find({});
+        console.log(notes);
         res.render('dashboard/index', {
             locals,
-            layout:'../views/layouts/dashboard'
+            notes,
+            layout: '../views/layouts/dashboard'
         });
+
+    } catch (error) {
+        console.log(error)
+
     }
-    
+
+
+}
